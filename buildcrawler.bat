dac4c4a96055de84ad1e3d8577a5f4ae93a68444
jinglu-jlu <jinglu.jlu@gmail.com>
2021-09-02 22:55:59 -0400

    rename activate.bat to activatecrawler.bat

--- buildcrawler.bat
+++ buildcrawler.bat
@@ -6,6 +6,6 @@ crawler\Scripts\pip.exe install requests
 crawler\Scripts\pip.exe install bs4
 crawler\Scripts\pip.exe install lxml
 REM Create activate.bat
-echo "crawler\Scripts\activate.bat" > activate.bat
+echo "crawler\Scripts\activate.bat" > activatecrawler.bat
 REM Activate virtualenv
 crawler\Scripts\activate.bat
